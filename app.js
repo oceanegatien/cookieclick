2e50a379d17fcfa9798c278d66ce82a9a0799629
oceane <oceane.gatien.cef@hotmail.fr>
2017-03-29 17:10:27 +0200

    Add phrases aux multiple de 5

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
 	var count = 0;
+	var message = ['oui', 'non', 'maybe', 'dont known']
 	$('#img').on('click', function() {
 		count++;
 		$("#counter").html('nombre de clic: ' + count);
+		//console.log(count);
+		var random = Math.round(Math.random()*3);
+		console.log(random);
+		if (count %5 === 0) {
+		$('#message').html(message[random]);
+			
+		}
 	});
+
+
+$("#img").on({
+	'click': function() {
+		$("img").attr('src','http://orig00.deviantart.net/d02a/f/2013/360/3/c/kawaii_cookie_png_by_adminwookielover-d6zhcvd.png');
+	}
+})
 });
-
-
 
 
 
